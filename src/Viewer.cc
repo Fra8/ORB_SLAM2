665a84c397fec9b5572e2b8f008db6d71306f76d
Fra8 <carola.motolese@gmail.com>
2017-03-15 14:01:57 +0100

    Update Viewer.cc with DepthMapFactor slider

--- src/Viewer.cc
+++ src/Viewer.cc
@@ -73,8 +73,7 @@ void Viewer::Run()
     pangolin::Var<bool> menuLocalizationMode("menu.Localization Mode",false,true);
     pangolin::Var<int> menuDepth("menu.DepthMapFactor", mpTracker->mdmf, 0,5000); // Variable to set DepthMapFactor 
     pangolin::Var<bool> menuReset("menu.Reset",false,false);
-   
-     
+       
     // Define Camera Render Object (for view / scene browsing)
     pangolin::OpenGlRenderState s_cam(
                 pangolin::ProjectionMatrix(1024,768,mViewpointF,mViewpointF,512,389,0.1,1000),
